576d81824f51d1ad484ce43a471cefaeff332b23
Nicholas "LB" Braden <LB@LB-Stuff.com>
2014-08-11 07:14:28 -0500

    Fix bugs, change cancellable semantics, etc

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ struct TestEvent
 : resplunk::event::EventImplementor<TestEvent, resplunk::event::CancellableEvent>
 {
 	int x;
-	TestEvent(resplunk::server::Server &s, int x = 7)
+	TestEvent(resplunk::server::Server &s, int x)
 	: resplunk::server::ServerSpecific(s)
 	, x(x)
 	{
@@ -15,17 +15,38 @@ struct TestEvent
 };
 
 struct TestListener
-: resplunk::event::EventReactor<TestEvent>
+: resplunk::event::EventProcessor<TestEvent>
+, resplunk::event::EventReactor<TestEvent>
 {
 	TestListener(resplunk::server::Server &s)
-		: resplunk::server::ServerSpecific(s)
-	, resplunk::event::EventReactor<TestEvent>(0)
+	: resplunk::server::ServerSpecific(s)
 	{
 	}
 
+	virtual void onEvent(TestEvent &e) const override
+	{
+		if(e.x > 10)
+		{
+			e.x = 10;
+		}
+		else if(e.x < 0)
+		{
+			e.setCancelled(true);
+		}
+	}
 	virtual void onEvent(TestEvent const &e) override
 	{
 		std::cout << "x = " << e.x << std::endl;
+	}
+};
+
+struct DerivedTestEvent
+: resplunk::event::EventImplementor<DerivedTestEvent, TestEvent>
+{
+	DerivedTestEvent(resplunk::server::Server &s)
+	: resplunk::server::ServerSpecific(s)
+	, TestEvent(s, 4)
+	{
 	}
 };
 
@@ -38,5 +59,8 @@ int main(int nargs, char **args)
 {
 	TestServer s;
 	TestListener l {s};
-	TestEvent{s}.call();
+	TestEvent{s, 7}.call();
+	TestEvent{s, -1}.call();
+	TestEvent{s, 14}.call();
+	DerivedTestEvent{s}.call();
 }
